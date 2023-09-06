7a3cda4bd3d288dac3a5aae9b650d58e9c633b61
sushmakurella <suahmakurella@gmail.com>
2023-09-06 20:56:18 +0530

    morechanges

--- src/main/java/com/springproject/demo/controller/QuestionController.java
+++ src/main/java/com/springproject/demo/controller/QuestionController.java
@@ -19,7 +19,17 @@ import com.springproject.demo.model.StudentLogin;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+//******************************************************
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
 @Controller
 public class QuestionController {
 	@Autowired
